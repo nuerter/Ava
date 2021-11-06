2b06e14897dc735798d02f5057487f00908d68df
nuerter <ivan.stanchevv1@gmail.com>
2021-11-06 12:27:41 +0200

    uploading Ava to Hub

---
+++ Ava.py
@@ -0,0 +1,16 @@
+c="I'm pleased to meet you Ava"
+b="Do you have a name?"
+a=input("Please write your name ")
+line0 = input("SESSION 1")
+line1 = input("'Breaking the ice'")
+line2 = input("Ava: Hello.")
+line3 = input(a + ": Hi.")
+line4 = input(a + ": I'm " + a + ".")
+line5 = input("Ava: Hello " + a + ".")
+line6 = input(a + ": " + b)
+if b == str("Do you have a name?"):
+   print(input("Ava: Yes, Ava."))
+if c == str("I'm pleased to meet you Name"):
+   line7 = input(a + ": " + c)
+else:
+   print(input(a + ": " + c))

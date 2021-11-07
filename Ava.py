c4191fd55d80a3d5e4115c3479de2d8aa2e02f36
nuerter <ivan.stanchevv1@gmail.com>
2021-11-07 19:20:13 +0200

    Ava full

--- Ava.py
+++ Ava.py
@@ -1,16 +1,30 @@
-c="I'm pleased to meet you Ava"
-b="Do you have a name?"
-a=input("Please write your name ")
-line0 = input("SESSION 1")
-line1 = input("'Breaking the ice'")
-line2 = input("Ava: Hello.")
-line3 = input(a + ": Hi.")
-line4 = input(a + ": I'm " + a + ".")
-line5 = input("Ava: Hello " + a + ".")
-line6 = input(a + ": " + b)
+a = input("Please write your name ")
+h = a + ": So we need to brake the ice."
+g = ": None like you."
+f = "Ava: Haven't you met lots of new people before?"
+e = a + ": Then i guess we're both in quite a similar position."
+def line_13():
+    return(print(h))
+
+c = "I'm pleased to meet you Ava."
+b = "Do you have a name?"
+line_0 = input("SESSION 1")
+line_1 = input("'Breaking the ice'")
+line_2 = input("Ava: Hello.")
+line_3 = input(a + ": Hi.")
+line_4 = input(a + ": I'm " + a + ".")
+line_5 = input("Ava: Hello " + a + ".")
+line_6 = input(a + ": " + b)
 if b == str("Do you have a name?"):
-   print(input("Ava: Yes, Ava."))
-if c == str("I'm pleased to meet you Name"):
-   line7 = input(a + ": " + c)
+    print(input("Ava: Yes, Ava."))
+if c == str("I'm pleased to meet you Basic Python"):
+    line_7 = input(a + ": " + c)
 else:
-   print(input(a + ": " + c))
+    print(input(a + ": " + c))
+line_8 = input("Ava: I'm pleased to meet you too.")
+line_9 = input("Ava: I've never met anyone new before. Only Nathan.")
+line_10 = input(e)
+line_11 = input(f[:])
+line_12 = input(a + g)
+line_13()
+line_14 = input("               THE END")

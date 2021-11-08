cb2be96d5b5a0a2003be0ce58100822f7ba65df0
nuerter <ivan.stanchevv1@gmail.com>
2021-11-08 11:45:26 +0200

    renamed variables

--- Ava.py
+++ Ava.py
@@ -1,30 +1,30 @@
-a = input("Please write your name ")
-h = a + ": So we need to brake the ice."
-g = ": None like you."
-f = "Ava: Haven't you met lots of new people before?"
-e = a + ": Then i guess we're both in quite a similar position."
+Name = input("Please write your name ")
+script_ice = Name + ": So we need to brake the ice."
+script_none = ": None like you."
+script_people = "Ava: Haven't you met lots of new people before?"
+script_position = Name + ": Then i guess we're both in quite a similar position."
 def line_13():
-    return(print(h))
+    return(print(script_ice))
 
-c = "I'm pleased to meet you Ava."
-b = "Do you have a name?"
+script_meet = "I'm pleased to meet you Ava."
+script_name = "Do you have a name?"
 line_0 = input("SESSION 1")
 line_1 = input("'Breaking the ice'")
 line_2 = input("Ava: Hello.")
-line_3 = input(a + ": Hi.")
-line_4 = input(a + ": I'm " + a + ".")
-line_5 = input("Ava: Hello " + a + ".")
-line_6 = input(a + ": " + b)
-if b == str("Do you have a name?"):
+line_3 = input(Name + ": Hi.")
+line_4 = input(Name + ": I'm " + Name + ".")
+line_5 = input("Ava: Hello " + Name + ".")
+line_6 = input(Name + ": " + script_name)
+if script_name == str("Do you have a name?"):
     print(input("Ava: Yes, Ava."))
-if c == str("I'm pleased to meet you Basic Python"):
-    line_7 = input(a + ": " + c)
+if script_meet == str("I'm pleased to meet you Basic Python"):
+    line_7 = input(Name + ": " + script_meet)
 else:
-    print(input(a + ": " + c))
+    print(input(Name + ": " + script_meet))
 line_8 = input("Ava: I'm pleased to meet you too.")
 line_9 = input("Ava: I've never met anyone new before. Only Nathan.")
-line_10 = input(e)
-line_11 = input(f[:])
-line_12 = input(a + g)
+line_10 = input(script_position)
+line_11 = input(script_people[:])
+line_12 = input(Name + script_none)
 line_13()
 line_14 = input("               THE END")
